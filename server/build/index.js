a5606faa5f1e4121fbe845ca58e3005c30707545
umang-parmar <umang2502parmar@gmail.com>
2026-07-18 18:43:40 +0530

    Basic Server Setup added

---
+++ server/build/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("My Name Is Umang Parmar");
+// manually add scripts for ts to js code direct write and run in build folder  : Package.json
+//# sourceMappingURL=index.js.map
---
+++ server/src/index.ts
@@ -0,0 +1,2 @@
+console.log("My Name Is Umang Parmar")
+  // manually add scripts for ts to js code direct write and run in build folder  : Package.json
